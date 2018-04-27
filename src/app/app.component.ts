45ad61ff60e7f7949ac89791031ba7297938babd
Manuel Valles <manukempo@gmail.com>
2018-04-27 19:10:03 +0200

    Assignment Databinding - Solved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  servers = [];
+  newServerName: string;
+  newServerContent: string;
+
+  onAddServer(){
+    this.servers.push({
+      type: 'server',
+      name: this.newServerName, 
+      content: this.newServerContent
+    });
+  }
+  onAddBlueprint(){
+    this.servers.push({
+      type: 'blueprint',
+      name: this.newServerName, 
+      content: this.newServerContent});
+  }
 }
