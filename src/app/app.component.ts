5f009381454de4e443b44343d5ad38236159d243
Manuel Valles <manukempo@gmail.com>
2018-04-27 09:13:34 +0200

    Solution Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .fifth{
+      color: white;
+    }
+  `]
 })
 export class AppComponent {
-  
+  showSecret = false;
+  logs=[];
+  onToggleDetails(){
+    this.showSecret = !this.showSecret;
+    // this.logs.push(this.logs.length+1);
+    this.logs.push(new Date());
+  }
 }
