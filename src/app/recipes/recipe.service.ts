38c5b9231c746d7172f3104d663fd3711c568b05
Manuel Valles <manukempo@gmail.com>
2018-04-30 17:34:49 +0200

    Added Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,29 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   
   private recipes: Recipe [] = [
-    new Recipe('A Test Recipe1', 'This is simply a test1', 'https://www.maxpixel.net/static/photo/1x/Burgers-Burger-Chickpeas-Recipes-Food-Vegetables-2920072.jpg'),
-    new Recipe('A Test Recipe2', 'This is simply a test2', 'https://www.maxpixel.net/static/photo/1x/Burgers-Burger-Chickpeas-Recipes-Food-Vegetables-2920072.jpg')
+    new Recipe(
+      'A Test Recipe1', 
+      'This is simply a test1',
+      'https://www.maxpixel.net/static/photo/1x/Burgers-Burger-Chickpeas-Recipes-Food-Vegetables-2920072.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Fries', 20)
+      ]
+    ),
+    new Recipe(
+      'A Test Recipe2', 
+      'This is simply a test2',
+      'https://www.maxpixel.net/static/photo/1x/Burgers-Burger-Chickpeas-Recipes-Food-Vegetables-2920072.jpg',
+      [
+        new Ingredient('Bread', 2),
+        new Ingredient('Cheese', 1)
+      ]
+    )
   ];
 
   getRecipes(){
