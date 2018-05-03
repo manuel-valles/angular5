5ced3f6587c6d0d89d2d789372829733672544c3
Manuel Valles <manukempo@gmail.com>
2018-05-03 09:48:57 +0200

    Practicing Query Parameters

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -13,7 +13,8 @@ export class ServerComponent implements OnInit {
 
   constructor(
     private serversService: ServersService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
     // + converts it to a number
@@ -26,6 +27,10 @@ export class ServerComponent implements OnInit {
           this.server = this.serversService.getServer(+params['id']);
         }
       )
+  }
+
+  onEdit(){
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling:'preserve'});
   }
 
 }
