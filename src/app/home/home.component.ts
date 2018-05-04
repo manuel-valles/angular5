5621aa6956c784a7cdec9c26807033e097709610
Manuel Valles <manukempo@gmail.com>
2018-05-04 19:15:17 +0200

    Unsubscribe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/Rx';
 
 @Component({
@@ -7,19 +9,51 @@ import 'rxjs/Rx';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  numbersObsSubscription: Subscription;
+  customObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
     // Create Observable with helper method - interval(milsec)
+
     const myNumbers = Observable.interval(1000);
-    myNumbers.subscribe(
+    this.numbersObsSubscription = myNumbers.subscribe(
       (number: number) => {
         console.log(number);
       }
-      
+    );
+
+
+    // Create Observable from scratch with the method create(function)
+    // This function holds the asynchronous code
+    const myObservable = Observable.create(
+      (observer: Observer<string>)=>{
+        setTimeout(()=>{
+          observer.next('first package');
+        }, 2000);
+        setTimeout(()=>{
+          observer.next('second package');
+         }, 4000);
+        setTimeout(()=>{
+          // observer.error('this does not work');
+          observer.complete();
+         }, 5000);
+        setTimeout(()=>{
+          observer.next('third package');
+         }, 5000);
+    });
+    this.customObsSubscription = myObservable.subscribe(
+      (data: string) => {console.log(data);},
+      (error: string) => {console.log(error);},
+      () => {console.log('completed')}
     );
   }
 
+  ngOnDestroy(){
+    this.numbersObsSubscription.unsubscribe();
+    this.customObsSubscription.unsubscribe();
+  }
 }
