8118320c711109719c072ca9cd3e5f194e711b23
Manuel Valles <manukempo@gmail.com>
2018-05-06 08:25:01 +0200

    Using Form Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -39,6 +47,11 @@ export class AppComponent {
 */
 
   onSubmit(){
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.form.value.userData.username;
+    this.user.email = this.signupForm.form.value.userData.email;
+    this.user.secretQuestion = this.signupForm.form.value.secret;
+    this.user.answer = this.signupForm.form.value.questionAnswer;
+    this.user.gender = this.signupForm.form.value.gender;
   }
 }
