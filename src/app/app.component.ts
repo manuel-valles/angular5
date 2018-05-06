45a61a5df2b5d460000d52932d12a735424a6cee
Manuel Valles <manukempo@gmail.com>
2018-05-06 11:53:47 +0200

    Custom Validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,12 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Mar', 'Manu'];
 
   ngOnInit(){
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -29,5 +30,15 @@ export class AppComponent implements OnInit{
     const control = new FormControl(null, Validators.required);
     // Need to specify the type and close in () to be treated as an Array
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  // A Validator is just a function that must receive a FormControl
+  // and retrieve an object with a key-value pair string-boolean
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if(this.forbiddenUsernames.indexOf(control.value) !== -1){
+      return {'nameIsForbidden': true};
+    }
+    // Return nothing or return null to say Angular that the FormControl is valid
+    return null;
   }
 }
