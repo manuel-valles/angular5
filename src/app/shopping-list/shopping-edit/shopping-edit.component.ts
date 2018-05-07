41c3d7e86a6397b7084b2dbb0d931d90ded617c3
Manuel Valles <manukempo@gmail.com>
2018-05-07 12:52:52 +0200

    Updating Shopping List

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild} from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingService } from '../shopping.service';
@@ -9,16 +10,41 @@ import { ShoppingService } from '../shopping.service';
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f') shoppingForm: NgForm;
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+  editedItem: Ingredient;
 
   constructor(private shoppingService: ShoppingService) { }
 
   ngOnInit() {
+    this.subscription = this.shoppingService.startedEditing
+      .subscribe(
+        (index: number) => {
+          this.editMode = true;
+          this.editedItemIndex = index;
+          this.editedItem = this.shoppingService.getIngredient(index);
+          this.shoppingForm.setValue({
+            name: this.editedItem.name,
+            amount: this.editedItem.amount
+          });
+        }
+      );
   }
 
   onAddItem(form: NgForm){
     const value = form.value;
-    const newIngredient = new Ingredient(value.name, value.amount); 
-    this.shoppingService.addIngredient(newIngredient);
+    const newIngredient = new Ingredient(value.name, value.amount);
+    if(this.editMode){
+      this.shoppingService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else{
+      this.shoppingService.addIngredient(newIngredient);
+    }
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 }
