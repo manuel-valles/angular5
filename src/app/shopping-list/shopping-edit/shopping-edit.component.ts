cde3b2356d6a7fa562572750bc748b3d73759f3b
Manuel Valles <manukempo@gmail.com>
2018-05-07 13:08:41 +0200

    Resetting Shoping List Form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -34,7 +34,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       );
   }
 
-  onAddItem(form: NgForm){
+  onSubmit(form: NgForm){
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if(this.editMode){
@@ -42,6 +42,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else{
       this.shoppingService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    this.shoppingForm.reset();
   }
 
   ngOnDestroy(){
