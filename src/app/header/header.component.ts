b281b476a532b963ba637d079c20a5c853906835
Manuel Valles <manukempo@gmail.com>
2018-05-08 18:13:10 +0200

    Sending Put Requests to Save Data

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +10,11 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
-
+  constructor(private dsService: DataStorageService){}
+  onSaveData(){
+    this.dsService.storeRecipes()
+      .subscribe(
+        (response: Response) => console.log(response)
+      );
+  }
 }
